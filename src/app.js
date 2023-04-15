3de1afd542fec1e2a0cdc6270bb3d685aa118c1b
Lucas Nunes <120993347+thezunes@users.noreply.github.com>
2023-04-15 01:17:14 -0300

    requirement post /participants

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
 import joi from "joi"
+import dayjs from "dayjs"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 dotenv.config()
+
+
+
 
 let db
 const mongoClient = new MongoClient("mongodb://0.0.0.0:27017/uol")
@@ -17,9 +21,7 @@ mongoClient.connect()
 
     app.post("/participants", async (req, res) => {
         const { name } = req.body
-      
-        
-      
+
         const userSchema = joi.object({
           name: joi.string().required()
         });
@@ -36,14 +38,30 @@ mongoClient.connect()
         const names = await db.collection("participants").findOne({name: name})
         if(names) return res.status(422).send("Usuário já cadastrado")
 
-        const newParticipant = { name }
+        const newParticipant = { name, lastStatus: Date.now() }
+
+        const horaAtual = dayjs().format('HH:mm:ss');
+
 
         try {
+          
+          const time = dayjs().format('HH:mm:ss');
+          const userInfo = { 
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: time
+        }
+
           await db.collection("participants").insertOne(newParticipant)
           res.status(201).send("Pariticipante adicionado")
+          await db.collection("message").insertOne(userInfo)
+          
+
         }
         catch{ 
-          res.status(err).send(err)
+          res.status(404)
         }
     })
 
